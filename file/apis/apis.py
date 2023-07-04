ed1eb016fb6f21e064674a85e61c0591f53b40bc
Robinhood <okaforchidubem7@gmail.com>
2023-07-04 14:20:19 +0100

    apis

--- file/apis/apis.py
+++ file/apis/apis.py
@@ -2,16 +2,40 @@ import requests
 
 response = requests.get("http://api.publicapis.org/entries")
 res = response.json()
-
-name = input("enter a name: ")
+print()
+print("What are you looking for?")
+print("1. Link")
+print("2. Description")
+print("3. Category")
+print()
+number = int(input("Write a number: "))
 length = range(len(res["entries"]))
-for i in length:
-    if name == res["entries"][i]["API"]:
-        print(name)
-    else:
-        print("name does not exist")
-# if name in res["entries"][0]["Name"]:
-#     print(name)
-# print("no name")
-# content = res["entries"][0]["name"]
-# print(content)
+na = []
+
+
+class State:
+    def __init__(self, number):
+        self.number = number
+        if self.number == 1:
+            self.value = "Link"
+        elif self.number == 2:
+            self.value = "Description"
+        elif self.number == 3:
+            self.value = "Category"
+        else:
+            self.value = "API"
+
+    def getValue(self):
+        return self.value
+
+
+if number == 1:
+    name = input("Enter API name: ")
+
+    for i in length:
+        query = res["entries"][int(i)]["API"]
+        if name == query:
+            na.append(res["entries"][int(i)]["Link"])
+
+
+print(na)
