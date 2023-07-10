fbffb168a8fe5e1e0800935d8933845b50cd34c9
Robinhood <okaforchidubem7@gmail.com>
2023-07-10 12:32:55 +0100

    addes functionalities

--- api/customer.py
+++ api/customer.py
@@ -2,13 +2,42 @@ import requests
 
 url = "https://api-customer-038j.onrender.com/customer/"
 
-put_response = requests.put(
-    url + "2/", {"name": "mike", "age": "44", "order": "bread"})
-print(put_response.json())
+print("what do you want to do?")
+print("1. update_order")
+print("2. add_order")
+print("3. get orders")
+print("4. get with id")
+print("5. delete")
+print()
+number = int(input("write a number: "))
 
-get_response = requests.get(url)
-print(get_response.json())
+if number == 1:
+    id = int(input("what is your id: "))
+    name = input("name: ")
+    age = int(input("age: "))
+    order = input("order: ")
+    print()
+    put_response = requests.put(
+        url + f"{id}/", {f"name": {name}, "age": {age}, "order": {order}})
+    print(put_response.json())
 
-post_response = requests.post(
-    url, {"name": "daniel", "age": 29, "order": "rice"})
-print(post_response.json())
+if number == 2:
+    name = input("name: ")
+    age = int(input("age: "))
+    order = input("order: ")
+    print()
+    post_response = requests.post(
+        url, {f"name": {name}, "age": {age}, "order": {order}})
+    print(post_response.json())
+
+if number == 3:
+    get_response = requests.get(url)
+    print(get_response.json())
+
+if number == 4:
+    id = int(input("id of what you want to get"))
+    get_response = requests.get(url + f"{id}/")
+
+if number == 5:
+    id = int(input("id of what you want to delete: "))
+    get_response = requests.delete(url + f"{id}/")
